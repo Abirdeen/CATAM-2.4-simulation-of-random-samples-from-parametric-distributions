fef29f46d8ceef310163ab64fdac1e665f654c99
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2025-03-05 11:20:17 +0000

    feat: provide analysis of the gamma distribution

--- R/cdfs.R
+++ R/cdfs.R
@@ -26,3 +26,30 @@ exponential_distribution <- function(x = ? numeric, rate = ? numeric) {
 exponential_distribution_inv <- function(x = ? numeric, rate = ? numeric) {
   return(-log(1 - x) / rate)
 }
+
+
+#' CDF of the gamma distribution.
+#'
+#' @param x Non-negative float. The sample point.
+#' @param rate Positive float. The rate parameter.
+#' @param shape Positive float. The shape parameter.
+#' @return Float. If X~Gamma(rate, shape), this represents P(X<=`x`).
+#' @examples
+#' gamma_distribution(x, rate, 1) = exponential_distribution(x, rate)
+#' gamma_distribution(x, rate, 2) = 1 - e^(-rate * x) * (rate * x + 1)
+gamma_distribution <- function(x = ? numeric,
+                               rate = ? numeric, shape = ? numeric) {
+  return(1 - gammainc(shape, rate * x) / gamma(shape))
+}
+
+#' Inverse CDF of the gamme distribution of shape 2.
+#'
+#' @param x Non-negative float, 0<=`x`<=1. The sample point.
+#' @param rate Positive float. The rate parameter.
+#' @return Float. If X~Gamma(rate,2), this represents m such that P(X<=m) = `x`.
+#' @examples
+#' gamma_distribution_special_inv(0, rate) = 0
+#' gamma_distribution_special_inv(ye^(y+1) + 1, 1) = -1 - y
+gamma_distribution_special_inv <- function(x = ? numeric, rate = ? numeric) {
+  return(-(lambertWn((x - 1) / exp(1)) + 1) / rate)
+}
